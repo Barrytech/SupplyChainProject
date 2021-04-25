6cf58f35c774545d79fc16635dc20c47603a93ea
Barrytech <barryabdoul2024@gmail.com>
2021-04-25 03:16:32 -0400

    I have pulled back all the commit that contains bugs and stopped the application from running. The next step is to update the project step by step and track progress and where things start to break.

--- project-3/contracts/coffeebase/SupplyChain.sol
+++ project-3/contracts/coffeebase/SupplyChain.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity = 0.5.16;
 // Define a contract 'Supplychain'
 contract SupplyChain {
 
@@ -49,7 +49,7 @@ contract SupplyChain {
     State   itemState;  // Product State as represented in the enum above
     address distributorID;  // Metamask-Ethereum address of the Distributor
     address retailerID; // Metamask-Ethereum address of the Retailer
-    address consumerID; // Metamask-Ethereum address of the Consumer
+    address payable consumerID; // Metamask-Ethereum address of the Consumer
   }
 
   // Define 8 events with the same 8 state values and accept 'upc' as input argument
@@ -145,15 +145,9 @@ contract SupplyChain {
     upc = 1;
   }
 
-  // Define a function 'kill' if required
-  function kill() public {
-    if (msg.sender == owner) {
-      selfdestruct(owner);
-    }
-  }
 
   // Define a function 'harvestItem' that allows a farmer to mark an item 'Harvested'
-  function harvestItem(uint _upc, address _originFarmerID, string _originFarmName, string _originFarmInformation, string  _originFarmLatitude, string  _originFarmLongitude, string  _productNotes) public 
+  function harvestItem(uint _upc, address _originFarmerID, string memory _originFarmName, string memory _originFarmInformation, string memory  _originFarmLatitude, string memory  _originFarmLongitude, string memory  _productNotes) public 
   {
     // Add the new item as part of Harvest
     
@@ -269,10 +263,10 @@ contract SupplyChain {
   uint    itemUPC,
   address ownerID,
   address originFarmerID,
-  string  originFarmName,
-  string  originFarmInformation,
-  string  originFarmLatitude,
-  string  originFarmLongitude
+  string  memory originFarmName,
+  string  memory originFarmInformation,
+  string  memory originFarmLatitude,
+  string  memory originFarmLongitude
   ) 
   {
   // Assign values to the 8 parameters
@@ -297,12 +291,12 @@ contract SupplyChain {
   uint    itemSKU,
   uint    itemUPC,
   uint    productID,
-  string  productNotes,
+  string  memory productNotes,
   uint    productPrice,
   uint    itemState,
   address distributorID,
   address retailerID,
-  address consumerID
+  address payable consumerID
   ) 
   {
     // Assign values to the 9 parameters
